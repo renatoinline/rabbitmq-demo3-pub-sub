f33054dc18c244b09143bab7548ffdd0f036152b
Renato Colaco <renatocolaco@gmail.com>
2019-05-18 21:35:35 -0300

    Add project files.

---
+++ EmitLog/EmitLog.cs
@@ -0,0 +1,62 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Timers;
+
+namespace EmitLog
+{
+    class EmitLog
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            SetTimer();
+
+            Console.ReadKey();
+        }
+
+        private static void SetTimer()
+        {
+            // Create a timer with a two second interval.
+            var messageTimer = new Timer(2000);
+            // Hook up the Elapsed event for the timer. 
+            messageTimer.Elapsed += OnTimedEvent;
+            messageTimer.AutoReset = true;
+            messageTimer.Enabled = true;
+        }
+
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "192.168.15.33",
+                UserName = "renatocolaco",
+                Password = "secnet123"
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+
+                var message = GetMessage();
+                var body = Encoding.UTF8.GetBytes(message);
+                
+                channel.BasicPublish(exchange: "logs",
+                                     routingKey: "",
+                                     basicProperties: null,
+                                     body: body);
+            }
+
+            Console.WriteLine("A new message published to the queue at {0:HH:mm:ss.fff}",
+                              e.SignalTime);
+        }
+
+        private static string GetMessage()
+        {
+            return $"Hello World! {DateTime.Now:HH:mm:ss}";
+        }
+    }
+}
---
+++ ReceiveLogs/ReceiveLogs.cs
@@ -0,0 +1,56 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace ReceiveLogs
+{
+    class ReceiveLogs
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = "192.168.15.33",
+                UserName = "renatocolaco",
+                Password = "secnet123"
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                // This tells RabbitMQ not to give more than one message to a worker at a time
+                // prefetchCount: 1
+                channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+                var queueName = channel.QueueDeclare().QueueName;
+                channel.QueueBind(queue: queueName,
+                                  exchange: "logs",
+                                  routingKey: "");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) => {
+
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+
+                    Console.WriteLine(" [x] message received {0}", message);
+                    Thread.Sleep(4000);
+                    Console.WriteLine(" [#] Done");
+                };
+
+                channel.BasicConsume(
+                    queue: queueName,
+                    autoAck: true,
+                    consumer: consumer
+                    );
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
